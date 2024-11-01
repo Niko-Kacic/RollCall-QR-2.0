f556ad0bb037e251de89d94a738700a11fe57b38
NikoBoolean <niko.kacic@gmail.com>
2024-11-01 19:31:55 -0300

    Firebase is configured in the project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+   firebaseConfig : {
+    apiKey: "AIzaSyBWSYKfAUV474S-iSn9mGyFikDoy5xPzyc",
+    authDomain: "rollcall-qr-4a045.firebaseapp.com",
+    projectId: "rollcall-qr-4a045",
+    storageBucket: "rollcall-qr-4a045.firebasestorage.app",
+    messagingSenderId: "307572783998",
+    appId: "1:307572783998:web:dcaad4be65ef9906ecd12f"
+  }
 };
 
 /*
