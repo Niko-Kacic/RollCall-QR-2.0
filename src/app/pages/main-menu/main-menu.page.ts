565d1050ad463589799adb0c61519cbb1bc9039a
NikoBoolean <niko.kacic@gmail.com>
2024-11-02 19:59:21 -0300

    adjustment of the main menu and creation of the API service, routing still to be corrected

--- src/app/pages/main-menu/main-menu.page.ts
+++ src/app/pages/main-menu/main-menu.page.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { MenuController } from '@ionic/angular';
+import { NewsService } from 'src/app/services/news.service';
 
 @Component({
   selector: 'app-main-menu',
   templateUrl: './main-menu.page.html',
   styleUrls: ['./main-menu.page.scss'],
 })
+
 export class MainMenuPage implements OnInit {
 
-  constructor() { }
+  news: any[] = [];
+  events: any[] = [];
+
+  constructor(private menu: MenuController, private newsService: NewsService) { }
 
   ngOnInit() {
+    this.newsService.getNews().subscribe((data) => {
+      this.news = data.noticias;
+      this.events = data.eventos;
+    });
   }
 
 }
