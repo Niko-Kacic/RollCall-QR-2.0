42f0c5b451bfd5905fee830327fbec17eecae076
NikoBoolean <niko.kacic@gmail.com>
2024-11-05 17:38:00 -0300

    error children fixed

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -36,7 +36,7 @@ const routes: Routes = [
     path: 'subjects',
 
     loadChildren: () => import('./pages/subjects/subjects.module').then( m => m.SubjectsPageModule),
-    canActivate: [authGuard]
+    canActivate: [authGuard],
     children:[
       {
         path: '',
