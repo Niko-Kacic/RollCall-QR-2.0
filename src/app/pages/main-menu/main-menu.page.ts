e23a61371b1167a238838733589f32dfc448df84
Andhre Cruz <138709296+AndhreCruz@users.noreply.github.com>
2024-11-21 15:01:36 -0300

    Bringing changes from the improving-ui-nk branch

--- src/app/pages/main-menu/main-menu.page.ts
+++ src/app/pages/main-menu/main-menu.page.ts
@@ -3,6 +3,7 @@ import { Router } from '@angular/router';
 import { MenuController } from '@ionic/angular';
 import { AuthService } from 'src/app/services/auth.service';
 import { Preferences } from '@capacitor/preferences';
+import { FireDataBaseService } from 'src/app/services/fire-data-base.service';
 
 @Component({
   selector: 'app-main-menu',
@@ -13,16 +14,38 @@ import { Preferences } from '@capacitor/preferences';
 export class MainMenuPage implements OnInit {
 
   public footerTitle: string = '{ Code By CodeCrafters }';
+  public userName: string = ''; 
   darkModeEnabled: boolean = false;
 
   constructor(
     private menu: MenuController,
     private router: Router,
-    private authService: AuthService
+    private authService: AuthService,
+    private fireDataBaseService: FireDataBaseService
   ) {
     this.loadDarkModePreference();
    }
 
+   // Obtener el nombre del usuario autenticado
+   async ngOnInit() {
+    const user = await this.authService.getCurrentUser();
+    if (user && user.email) {
+      const email = user.email;
+      console.log('Correo del usuario autenticado:', email); 
+      this.fireDataBaseService.getUserDataByEmail(email).subscribe(data => {
+        if (data && data.name) {
+          this.userName = data.name; 
+          console.log('Nombre del usuario obtenido:', this.userName); 
+        } else {
+          console.log('No se encontró el usuario con el correo proporcionado');
+        }
+      });
+    } else {
+      console.log('No se pudo obtener el correo del usuario autenticado');
+    }
+  }
+
+  // Modo oscuro
    async loadDarkModePreference() {
     const { value } = await Preferences.get({ key: 'dark-mode' });
     this.darkModeEnabled = value === 'true';
@@ -39,6 +62,7 @@ export class MainMenuPage implements OnInit {
     document.body.classList.toggle('dark', this.darkModeEnabled);
   }
 
+  // Redirección a las páginas
   redirect_profile(){
     this.menu.close();
     this.router.navigate(['/profile']);
@@ -48,11 +72,13 @@ export class MainMenuPage implements OnInit {
     this.menu.close();
     this.router.navigate(['/subjects']);
   };
+
   redirect_404(){
     this.menu.close();
     this.router.navigate(['/error-404']);
   };
 
+  // Cerrar sesión
   async logout() {
     try {
       await this.authService.logout();
@@ -60,10 +86,6 @@ export class MainMenuPage implements OnInit {
     } catch (error) {
       console.error('Error al cerrar sesión:', error);
     }
-  }
-
-  ngOnInit() {
-
   }
 
 }
