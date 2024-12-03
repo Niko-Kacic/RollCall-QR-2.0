87d93e5f845ba909509fd4ff870b95a19d689c84
NikoBoolean <niko.kacic@gmail.com>
2024-12-03 16:21:17 -0300

    fixing

--- src/app/pages/subjects/subject-detail/subject-detail.page.ts
+++ src/app/pages/subjects/subject-detail/subject-detail.page.ts
@@ -2,7 +2,10 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
 import { SubjectsApiService } from 'src/app/services/subjects-api.service';
-import { CapacitorBarcodeScanner, CapacitorBarcodeScannerTypeHint } from '@capacitor/barcode-scanner';
+import {
+  CapacitorBarcodeScanner,
+  CapacitorBarcodeScannerTypeHint,
+} from '@capacitor/barcode-scanner';
 import { ToastController } from '@ionic/angular';
 
 @Component({
@@ -11,28 +14,29 @@ import { ToastController } from '@ionic/angular';
   styleUrls: ['./subject-detail.page.scss'],
 })
 export class SubjectDetailPage implements OnInit {
-
   subjectDetail: any;
   subjects: any[] = [];
   result: string = '';
   subjectAsist: number = 0;
   subjectPorcentage: number = 0;
-  public footerTitle: string = '{ Code By CodeCrafters }';
-  apiUrl: string = 'http://localhost:3000/courses';  // Ajusta la URL según sea necesario
+  footerTitle: string = '{ Code By CodeCrafters }';
+  apiUrl: string = 'http://localhost:3000/courses';
 
   constructor(
     private activatedrouter: ActivatedRoute,
     private http: HttpClient,
     private subjetApi: SubjectsApiService,
     private toastController: ToastController
-  ) { }
+  ) {}
 
   ngOnInit() {
-    this.activatedrouter.paramMap.subscribe(paramMap => {
+    this.activatedrouter.paramMap.subscribe((paramMap) => {
       const subjectId = paramMap.get('placeId');
       this.subjetApi.getSubjects().subscribe((data) => {
         this.subjects = data;
-        this.subjectDetail = this.subjects.find(signature => signature.id === subjectId);
+        this.subjectDetail = this.subjects.find(
+          (signature) => signature.id === subjectId
+        );
         this.subjectAsist = this.subjectDetail.asistencias;
         this.subjectPorcentage = this.subjectDetail.attendanceRate;
         console.log(this.subjectDetail);
@@ -52,7 +56,7 @@ export class SubjectDetailPage implements OnInit {
 
   async openCamera(): Promise<void> {
     const result = await CapacitorBarcodeScanner.scanBarcode({
-      hint: CapacitorBarcodeScannerTypeHint.ALL
+      hint: CapacitorBarcodeScannerTypeHint.ALL,
     });
     this.result = result.ScanResult;
 
@@ -70,28 +74,65 @@ export class SubjectDetailPage implements OnInit {
     this.subjectDetail.asistencias = this.subjectAsist;
 
     // Actualiza la asistencia en la API
-    this.http.put(`${this.apiUrl}/${this.subjectDetail.id}/asistencias`, { asistencias: this.subjectAsist })
-      .subscribe(response => {
-        console.log('Asistencias actualizadas:', response);
-      }, error => {
-        console.error('Error al actualizar asistencias:', error);
-      });
+    this.http
+      .put(`${this.apiUrl}/${this.subjectDetail.id}/asistencias`, {
+        asistencias: this.subjectAsist,
+      })
+      .subscribe(
+        (response) => {
+          console.log('Asistencias actualizadas:', response);
+        },
+        (error) => {
+          console.error('Error al actualizar asistencias:', error);
+        }
+      );
   }
 
   calculatePercentage() {
     if (this.subjectDetail.totalClasses > 0) {
-      this.subjectPorcentage = parseFloat(((this.subjectAsist / this.subjectDetail.totalClasses) * 100).toFixed(2));
+      this.subjectPorcentage = parseFloat(
+        ((this.subjectAsist / this.subjectDetail.totalClasses) * 100).toFixed(0)
+      );
       this.subjectDetail.attendanceRate = this.subjectPorcentage;
 
       // Actualiza el porcentaje de asistencia en la API
-      this.http.put(`${this.apiUrl}/${this.subjectDetail.id}/attendanceRate`, { attendanceRate: this.subjectPorcentage })
-        .subscribe(response => {
-          console.log('Porcentaje de asistencia actualizado:', response);
-        }, error => {
-          console.error('Error al actualizar el porcentaje de asistencia:', error);
-        });
+      this.http
+        .put(`${this.apiUrl}/${this.subjectDetail.id}/attendanceRate`, {
+          attendanceRate: this.subjectPorcentage,
+        })
+        .subscribe(
+          (response) => {
+            console.log('Porcentaje de asistencia actualizado:', response);
+          },
+          (error) => {
+            console.error(
+              'Error al actualizar el porcentaje de asistencia:',
+              error
+            );
+          }
+        );
     } else {
       this.subjectPorcentage = 0;
     }
+  }
+
+  updateBoth() {
+    this.subjetApi
+      .updateBoth(
+        this.subjectDetail.id,
+        this.subjectAsist,
+        this.subjectPorcentage
+      )
+      .subscribe(
+        (response) => {
+          console.log(
+            'Asistencia y porcentaje de asistencia actualizados:',
+            response
+          );
+        },
+        (error) => {
+          console.error('Error al actualizar asistencia y porcentaje:', error);
+        }
+      );
   }
 }
